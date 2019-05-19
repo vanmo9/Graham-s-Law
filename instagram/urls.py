64cd9d1cb2ce90c2aa6a23917ecc7b9609870ca0
mohassan <vanmowha@gmail.com>
2019-05-19 09:46:04 +0300

    a logout view function provided by the django built-in authentication system.

--- instagram/urls.py
+++ instagram/urls.py
@@ -19,5 +19,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^logout/$', views.logout, {"next_page": '/'}), 
     url(r'',include('gram.urls'))
 ]
