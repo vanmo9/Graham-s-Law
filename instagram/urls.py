11c300019d293b6ef08a8e6806fb3dbbd2823e3b
mohassan <vanmowha@gmail.com>
2019-05-19 09:46:44 +0300

    create the logout functionality

--- instagram/urls.py
+++ instagram/urls.py
@@ -20,5 +20,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^logout/$', views.logout, {"next_page": '/'}), 
-    url(r'',include('gram.urls'))
+    url(r'',include('gram.urls')) 
 ]
