5a15dd3a894c271efa5d80a572e887a2ef02138b
mohassan <vanmowha@gmail.com>
2019-05-19 11:17:31 +0300

    impoert forms

--- gram/forms.py
+++ gram/forms.py
@@ -1,2 +1,16 @@
 from django import forms
 from .models import *
+from pyuploadcare.dj.forms import ImageField
+
+
+class ProfileForm(forms,ModelsForm):
+    class Meta:
+        bio = forms.CharField(label = "Bio")
+        pic = ImageField(label = "Pic")
+
+
+class ImageForm(forms.ModelForm):
+    image_url = ImageField(label='Picture')
+    class Meta:
+        model = Image
+        fields = ("image_url","name","caption")
--- gram/views.py
+++ gram/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import *
+from .forms import *
 
 
 # Create your views here.
