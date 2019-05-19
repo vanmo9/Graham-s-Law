2429224f7576a89d798d8cbc529036ab149cfb52
mohassan <vanmowha@gmail.com>
2019-05-19 11:17:41 +0300

    models

--- gram/views.py
+++ gram/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .models import *
-from .forms import *
+from .forms import * 
 
 
 # Create your views here.
